abe6c9e68b99d8c7a3eaeab292d1de8db701357a
jhonnyzaruma95 <jhonnyz1995@hotmail.com>
2026-09-24 22:53:32 -0500

    agrego otra funcion calcularCapacidadPago

--- funciones.js
+++ funciones.js
@@ -1,8 +1,13 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
 function calcularDisponible(ingresos, egresos){
-    let disponible = ingresos - egresos;
-    if (disponible < 0){
+    let montoDisponible = ingresos - egresos;
+    if (montoDisponible < 0){
         return 0;
     }
-    return disponible;
+    return montoDisponible;
+}
+
+function calcularCapacidadPago(montoDisponible){
+    const capacidadPago = montoDisponible * 0.50;
+    return capacidadPago;
 }
--- simulador.js
+++ simulador.js
@@ -2,6 +2,12 @@
 function calcular(){
     let ingresos = parseFloat(document.getElementById("txtIngresos").value);
     let egresos = parseFloat(document.getElementById("txtEgresos").value);
-    let disponible = calcularDisponible(ingresos, egresos);
-    document.getElementById("spnDisponible").textContent = "USD " + disponible.toFixed(2);
+    let montoDisponible = calcularDisponible(ingresos, egresos);
+    document.getElementById("spnDisponible").textContent = "USD " + montoDisponible.toFixed(2);
+
+    let capacidadPago=calcularCapacidadPago(montoDisponible);
+    document.getElementById("spnCapacidadPago").textContent = "USD " + capacidadPago.toFixed(2);
 }
+
+
+
